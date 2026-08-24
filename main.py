a54271a222f0994dd81aa0e2b13e1d13f01e4b01
asClement <c1718117@gmail.com>
2026-08-24 23:23:06 +0000

    Debut 2.0-exploration.ipynb

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from src.split import split
+
 def main():
     print("Hello from fraud-detection!")
 
+    print()
+    split("data/train.csv", nb_period=75)
 
 if __name__ == "__main__":
     main()
